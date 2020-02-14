a955d16e988f9b47b6d2eeed408a47da56b0cc83
Dakota Jones <44828982+jonesdl17@users.noreply.github.com>
2020-02-13 22:20:09 -0600

    Update board.py

--- board.py
+++ board.py
@@ -1,23 +1,30 @@
 import pygame
+from King import King
 
-def build_game_board(width, height):
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.init()
-    pygame.display.set_caption("Chess")
-    pygame.display.get_surface().fill((255,255,255))
-    pygame.display.update()
-    isWhite = True
+width = 600
+height = 600
+pygame.display.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Chess")
+pygame.display.get_surface().fill((255,255,255))
+pygame.display.update()
+isWhite = True
 
-    for y in range(8):
-        for x in range(8):
-            color = [255,255,255] if isWhite else [0,0,0]
-            pygame.draw.rect(screen, color, pygame.Rect(x * int(width/8), y * int(height/8), int(width/8), int(height/8)))
-            isWhite = not isWhite
+for y in range(8):
+    for x in range(8):
+        color = [255,255,255] if isWhite else [0,0,0]
+        pygame.draw.rect(screen, color, pygame.Rect(x * int(width/8), y * int(height/8), int(width/8), int(height/8)))
         isWhite = not isWhite
+    isWhite = not isWhite
 
-    pygame.display.update() 
+pygame.display.update() 
 
-build_game_board(600, 600)
+board = [[0] * 8] * 8
+
+board[0][0] = King("King")
+king_piece = pygame.image.load(board[0][0].image)
+screen.blit(king_piece, (100, 200))
+print("image loaded")
 
 blnExitGame = False
 while not blnExitGame:
@@ -25,6 +32,3 @@ while not blnExitGame:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             blnExitGame = True
-        elif event.type == pygame.VIDEORESIZE:
-            width, height = pygame.display.get_window_size()
-            build_game_board(width, height)
