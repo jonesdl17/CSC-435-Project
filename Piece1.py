ddc078555529a261eae4b560679e016f648de1c2
lunajrr <lunajuan9966@gmail.com>
2020-02-16 00:46:26 -0600

    Changed Pieces file
    Having seperate files for each chess piece seems a little overkill. I think to reduce the amount of imports we have on the board file; we should use this structure. It would also be easier to make small adjustments if we adjust the parent class.

---
+++ Piece1.py
@@ -0,0 +1,42 @@
+import pygame
+#I think it oragnizes the pieces better, so we don't have to import a ton of imports on board
+#true = white pieces, False = Black pieces
+
+class Piece:
+    imageSize = [75,75]
+    def __init__(self, name):
+        self.name = name
+
+class King(Piece):
+    def __init__(self, name):
+        super().__init__("King")
+        self.image = "./King.png"
+       
+
+class Queen(Piece):
+    def __init__(self, name):
+        super().__init__("Queen")
+        self.image = "./Queen.png"
+       
+
+class Knight(Piece):
+    def __init__(self, name):
+        super().__init__("Knight")
+        self.image = "./Knight.png"
+       
+
+class Bishop(Piece):
+    def __init__(self, name):
+        super().__init__("Bishop")
+        self.image = "./Bishop.png"
+
+class Rook(Piece):
+    def __init__(self, name):
+        super().__init__("Rook")
+        self.image = "./Rook.png"
+
+class Pawn(Piece):
+    def __init__(self, name):
+        super().__init__("Pawn")
+        self.image = "./Pawn.png"
+       
