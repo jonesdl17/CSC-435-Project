90a02848b282898859e0d89377a30c8c94aefb0b
Dakota Jones <44828982+jonesdl17@users.noreply.github.com>
2020-02-24 23:51:58 -0600

    Update board.py

--- board.py
+++ board.py
@@ -4,12 +4,14 @@ from Square import *
 screen = 0
 width = 600
 height = 600
-king_piece = 0
+border_distance = 35
+king_piece = None
 king_piece_rect = None
-board_image = 0
-image_width = int(width/8)
-image_height = int(height/8)
+board_image = None
+image_width = int((width - border_distance * 2)/8)
+image_height = int((height - border_distance * 2)/8)
 board = [[0] * 8] * 8
+dragging = False
 
 def draw_board():
     global screen, image_width, image_height, board_image
@@ -40,13 +42,39 @@ def update_king(x, y):
     pygame.display.update()
 
 def move_piece():
-    global screen, board, king_piece
+    global screen, board, king_piece, dragging
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
-    drag = 0
 
     if click[0] == 1 and king_piece_rect.collidepoint((mouse[0], mouse[1])):
         update_king(mouse[0] - image_width/2, mouse[1] - image_height/2)
+        dragging = True
+
+    #This is really bad way to get the piece to fit in the square but it works for now and I will fix it later.
+    if click[0] == 0 and dragging:
+        x = mouse[0]
+        x -= border_distance
+        x = int(x / image_width)
+        x = x * image_width
+        x += border_distance
+        if x >= (width - border_distance):
+            x = width - border_distance - image_width
+        elif x <= border_distance:
+            x = border_distance
+
+        y = mouse[1]
+        y -= border_distance
+        y = int(y / image_height)
+        y = y * image_height
+        y += border_distance
+
+        if y >= (height - border_distance):
+            y = height - border_distance - image_height
+        elif y <= border_distance:
+            y = border_distance
+
+        update_king(x, y)
+        dragging = False
 
 def createSquares():
     i = 0
