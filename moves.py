19bca6de2de3d03f055fc503c3e9322ba69c3ef1
Dakota Jones <44828982+jonesdl17@users.noreply.github.com>
2020-03-03 23:05:41 -0600

    Add files via upload

--- moves.py
+++ moves.py
@@ -1,19 +1,104 @@
 def is_valid_move_pawn(board, source_tuple, target_tuple):
     is_valid = False
     is_in_board = target_tuple[0] < 8  and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
-    if source_tuple[0] == target_tuple[0] and target_tuple[1] == source_tuple[1] + 1 and is_in_board:
+    if source_tuple[0] == target_tuple[0] and target_tuple[1] == source_tuple[1] + 1 or target_tuple[1] == source_tuple[1] - 1 and is_in_board:
         is_valid = True
     return is_valid
 
 def is_valid_move_rook(board, source_tuple, target_tuple):
     is_valid = False
     is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
-    if target_tuple[0] == source_tuple[0] and target_tuple[1] != source_tuple[1]:
+    
+    if source_tuple[0] == target_tuple[0] and source_tuple[1] != target_tuple[1]:
         is_valid = True
-        for i in range(source_tuple[1] + 1, target_tuple[1] + 1):
-            is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
-    elif target_tuple[1] == source_tuple[1] and target_tuple[0] != source_tuple[0]:
+        if source_tuple[1] < target_tuple[1]:
+            for i in range(source_tuple[1] + 1, target_tuple[1]):
+                is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
+        else:
+            for i in range(target_tuple[1] + 1, source_tuple[1]):
+                is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
+    elif source_tuple[1] == target_tuple[1] and source_tuple[0] != target_tuple[0]:
         is_valid = True
-        for i in range(source_tuple[0] + 1, target_tuple[0] + 1):
-            is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+        if source_tuple[0] < target_tuple[0]:
+            for i in range(source_tuple[0] + 1, target_tuple[0]):
+                is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+        else:
+            for i in range(target_tuple[0] + 1, source_tuple[0]):
+                is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+    return is_valid
+
+def is_valid_move_bishop(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    if abs(target_tuple[0] - source_tuple[0]) == abs(target_tuple[1] - source_tuple[1]):
+        is_valid = True
+        multiplier = -1 if source_tuple[0] > target_tuple[0] else 1
+        col = source_tuple[0]
+        if source_tuple[1] < target_tuple[1]:
+            for i in range(source_tuple[1] + 1, target_tuple[1]):
+                col += 1 * multiplier
+                if board[col][i].piece != None:
+                    print(col, i)
+                is_valid = is_valid and (board[col][i].piece == None)
+        else:
+            for i in range(target_tuple[1] + 1, source_tuple[1]):
+                col += 1 * multiplier
+                if board[col][i].piece != None:
+                    print(col, i)
+                is_valid = is_valid and (board[col][i].piece == None)
+    return is_valid
+
+def is_valid_move_knight(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    
+    if target_tuple[1] == source_tuple[1] + 2 or target_tuple[1] == source_tuple[1] - 2:
+        if target_tuple[0] == source_tuple[0] + 1 or target_tuple[0] == source_tuple[0] -1:
+            is_valid = True
+    elif target_tuple[0] == source_tuple[0] + 2 or target_tuple[0] == source_tuple[0] - 2:
+        if target_tuple[1] == source_tuple[1] + 1 or target_tuple[1] == source_tuple[1] -1:
+            is_valid = True
+    return is_valid
+
+def is_valid_move_king(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    
+    if abs(target_tuple[0] - source_tuple[0]) == 1 or abs(target_tuple[1] - source_tuple[1]) == 1:
+        is_valid = True
+    return is_valid
+
+def is_valid_move_queen(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    
+    if source_tuple[0] == target_tuple[0] and source_tuple[1] != target_tuple[1]:
+        is_valid = True
+        if source_tuple[1] < target_tuple[1]:
+            for i in range(source_tuple[1] + 1, target_tuple[1]):
+                is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
+        else:
+            for i in range(target_tuple[1] + 1, source_tuple[1]):
+                is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
+    elif source_tuple[1] == target_tuple[1] and source_tuple[0] != target_tuple[0]:
+        is_valid = True
+        if source_tuple[0] < target_tuple[0]:
+            for i in range(source_tuple[0] + 1, target_tuple[0]):
+                is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+        else:
+            for i in range(target_tuple[0] + 1, source_tuple[0]):
+                is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+    
+    if abs(target_tuple[0] - source_tuple[0]) == abs(target_tuple[1] - source_tuple[1]):
+        is_valid = True
+        multiplier = -1 if source_tuple[0] > target_tuple[0] else 1
+        col = source_tuple[0]
+        if source_tuple[1] < target_tuple[1]:
+            for i in range(source_tuple[1] + 1, target_tuple[1] + 1):
+                col += 1 * multiplier
+                is_valid = is_valid and (board[col][i].piece == None)
+        else:
+            for i in range(target_tuple[1] + 1, source_tuple[1] + 1):
+                col += 1 * multiplier
+                is_valid = is_valid and (board[col][i].piece == None)
     return is_valid
