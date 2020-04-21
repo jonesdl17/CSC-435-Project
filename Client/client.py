749de47efdc5cfceb36ba5048ec9cbdbdbcef0c1
lunajrr <56463520+lunajrr@users.noreply.github.com>
2020-04-21 17:08:10 -0500

    Oragnizing files

---
+++ Client/client.py
@@ -0,0 +1,48 @@
+import socket
+import time
+
+soc = None
+PORT = 13456
+HOST = "127.0.0.1"
+server_address = (HOST, PORT)
+
+def get_color(gameMode):
+    return init_connect_to_server(gameMode)
+
+def init_connect_to_server(gameMode):
+    global soc
+    #Connect to the server socket
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.connect(server_address)
+
+    try:
+        data = gameMode
+        soc.send(b'%d' % (data))
+
+        returned_data = soc.recv(1024)
+        if data:
+            return int(returned_data)
+        else:
+            print("no data")
+    except Exception:
+        print("You either kill yourself or get killed!")
+
+def send_to_server(data):
+    global soc
+
+    soc.send(data.encode('utf-8'))
+
+def receive_from_server(response_lst):
+    global soc
+    try:
+        returned_data = soc.recv(1024)
+        if returned_data:
+            response_lst.append(returned_data.decode('utf-8'))
+        else:
+            print("no data")
+    except Exception:
+        print("You either kill yourself or get killed!")
+
+def close_socket():
+    global soc
+    soc.close()
---
+++ Client/moves.py
@@ -0,0 +1,107 @@
+from Piece import *
+def is_valid_move_pawn(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8  and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    if source_tuple[0] == target_tuple[0] and target_tuple[1] == source_tuple[1] + 2 and board[source_tuple[0]][source_tuple[1]].piece.checkFirstMove() and is_in_board or source_tuple[0] == target_tuple[0] and target_tuple[1] == source_tuple[1] - 2 and is_in_board and board[source_tuple[0]][source_tuple[1]].piece.checkFirstMove():
+        is_valid = True
+    elif source_tuple[0] == target_tuple[0] and target_tuple[1] == source_tuple[1] + 1 or source_tuple[0] == target_tuple[0] and target_tuple[1] == source_tuple[1] - 1 and is_in_board:
+        is_valid = True
+    return is_valid
+
+def is_valid_move_rook(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    
+    if source_tuple[0] == target_tuple[0] and source_tuple[1] != target_tuple[1]:
+        is_valid = True
+        if source_tuple[1] < target_tuple[1]:
+            for i in range(source_tuple[1] + 1, target_tuple[1]):
+                is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
+        else:
+            for i in range(target_tuple[1] + 1, source_tuple[1]):
+                is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
+    elif source_tuple[1] == target_tuple[1] and source_tuple[0] != target_tuple[0]:
+        is_valid = True
+        if source_tuple[0] < target_tuple[0]:
+            for i in range(source_tuple[0] + 1, target_tuple[0]):
+                is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+        else:
+            for i in range(target_tuple[0] + 1, source_tuple[0]):
+                is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+    return is_valid
+
+def is_valid_move_bishop(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    if abs(target_tuple[0] - source_tuple[0]) == abs(target_tuple[1] - source_tuple[1]):
+        is_valid = True
+        if source_tuple[1] < target_tuple[1]:
+            multiplier = -1 if source_tuple[0] > target_tuple[0] else 1
+            col = source_tuple[0]
+            for i in range(source_tuple[1] + 1, target_tuple[1]):
+                col += 1 * multiplier
+                is_valid = is_valid and (board[col][i].piece == None)
+        else:
+            multiplier = -1 if source_tuple[0] < target_tuple[0] else 1
+            col = target_tuple[0]
+            for i in range(target_tuple[1] + 1, source_tuple[1]):
+                col += 1 * multiplier
+                is_valid = is_valid and (board[col][i].piece == None)
+    return is_valid
+
+def is_valid_move_knight(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    
+    if target_tuple[1] == source_tuple[1] + 2 or target_tuple[1] == source_tuple[1] - 2:
+        if target_tuple[0] == source_tuple[0] + 1 or target_tuple[0] == source_tuple[0] -1:
+            is_valid = True
+    elif target_tuple[0] == source_tuple[0] + 2 or target_tuple[0] == source_tuple[0] - 2:
+        if target_tuple[1] == source_tuple[1] + 1 or target_tuple[1] == source_tuple[1] -1:
+            is_valid = True
+    return is_valid
+
+def is_valid_move_king(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    
+    if is_in_board and (abs(target_tuple[0] - source_tuple[0]) == 1 or abs(target_tuple[0] - source_tuple[0]) == 0) and (abs(target_tuple[1] - source_tuple[1]) == 1 or abs(target_tuple[1] - source_tuple[1]) == 0):
+        is_valid = True
+    return is_valid
+
+def is_valid_move_queen(board, source_tuple, target_tuple):
+    is_valid = False
+    is_in_board = target_tuple[0] < 8 and target_tuple[0] >= 0 and target_tuple[1] < 8 and target_tuple[1] >= 0 
+    
+    if source_tuple[0] == target_tuple[0] and source_tuple[1] != target_tuple[1]:
+        is_valid = True
+        if source_tuple[1] < target_tuple[1]:
+            for i in range(source_tuple[1] + 1, target_tuple[1]):
+                is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
+        else:
+            for i in range(target_tuple[1] + 1, source_tuple[1]):
+                is_valid = is_valid and (board[source_tuple[0]][i].piece == None)
+    elif source_tuple[1] == target_tuple[1] and source_tuple[0] != target_tuple[0]:
+        is_valid = True
+        if source_tuple[0] < target_tuple[0]:
+            for i in range(source_tuple[0] + 1, target_tuple[0]):
+                is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+        else:
+            for i in range(target_tuple[0] + 1, source_tuple[0]):
+                is_valid = is_valid and (board[i][source_tuple[1]].piece == None)
+    
+    if abs(target_tuple[0] - source_tuple[0]) == abs(target_tuple[1] - source_tuple[1]):
+        is_valid = True
+        if source_tuple[1] < target_tuple[1]:
+            multiplier = -1 if source_tuple[0] > target_tuple[0] else 1
+            col = source_tuple[0]
+            for i in range(source_tuple[1] + 1, target_tuple[1]):
+                col += 1 * multiplier
+                is_valid = is_valid and (board[col][i].piece == None)
+        else:
+            multiplier = -1 if source_tuple[0] < target_tuple[0] else 1
+            col = target_tuple[0]
+            for i in range(target_tuple[1] + 1, source_tuple[1]):
+                col += 1 * multiplier
+                is_valid = is_valid and (board[col][i].piece == None)
+    return is_valid
