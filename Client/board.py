8cd3f783d45b7eac1eb464014f6a8f3f1042fb8a
lunajrr <56463520+lunajrr@users.noreply.github.com>
2020-04-23 17:40:36 -0500

    Update board.py

--- Client/board.py
+++ Client/board.py
@@ -244,7 +244,7 @@ def find_valid_move_rook():
 def find_valid_move_pawn():
     global board, screen, source_col, source_row
     if color == 1:
-        if source_row + 2 < 8 and (board[source_col][source_row + 2].piece == None or board[source_col][source_row + 2].piece.color == other_color) and board[source_col][source_row].piece.checkFirstMove():
+        if source_row + 2 < 8 and (board[source_col][source_row + 2].piece == None or board[source_col][source_row + 2].piece.color == other_color) and board[source_col][source_row + 1].piece == None and board[source_col][source_row].piece.checkFirstMove():
             x, y = board[source_col][source_row + 2].getCoord()
             rec = pygame.Rect(x, y, image_width, image_height)
             pygame.draw.rect(screen, (0, 0, 255), rec, 1)
@@ -256,7 +256,7 @@ def find_valid_move_pawn():
             rec = pygame.Rect(x, y, image_width, image_height)
             pygame.draw.rect(screen, (0, 0, 255), rec, 1)
     elif color == 0:
-        if source_row - 2 > -1 and (board[source_col][source_row - 2].piece == None or board[source_col][source_row - 2].piece.color == other_color) and board[source_col][source_row].piece.checkFirstMove():
+        if source_row + 2 < 8 and (board[source_col][source_row + 2].piece == None or board[source_col][source_row + 2].piece.color == other_color) and board[source_col][source_row + 1].piece == None and board[source_col][source_row].piece.checkFirstMove():
             x, y = board[source_col][source_row - 2].getCoord()
             rec = pygame.Rect(x, y, image_width, image_height)
             pygame.draw.rect(screen, (0, 0, 255), rec, 1)
@@ -268,6 +268,7 @@ def find_valid_move_pawn():
             rec = pygame.Rect(x, y, image_width, image_height)
             pygame.draw.rect(screen, (0, 0, 255), rec, 1)
     pygame.display.update()
+
 
 def find_valid_move_bishop():
     global board, screen, source_col, source_row
